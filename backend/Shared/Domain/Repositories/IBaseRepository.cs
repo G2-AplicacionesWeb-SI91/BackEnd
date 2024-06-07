9bf146f7de2401d20357854a80bd919a09f2a74d
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-06-07 00:49:34 -0500

    feat(shared): added file Shared where resides the basic code for the project and packages dependencies.

---
+++ backend/Shared/Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,18 @@
+namespace backend.Shared.Domain.Repositories;
+
+public interface IBaseRepository<TEntity>
+{
+    //CRUD
+    //Insert
+    Task AddSync(TEntity entity);
+
+    //Get - Select
+    Task<TEntity?> FindByIdAsync(int id);
+    Task<IEnumerable<TEntity>> ListAsync();
+
+    //Update
+    void Update(TEntity entity);
+
+    //Delete
+    void Remove(TEntity entity);
+}
---
+++ backend/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -0,0 +1,19 @@
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        base.OnConfiguring(builder);
+        // Enable Audit Fields Interceptors
+        builder.AddCreatedUpdatedInterceptor();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
