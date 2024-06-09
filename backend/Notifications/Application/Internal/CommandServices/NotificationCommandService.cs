af68c78d4f28861d45f4dad2a88764899cd7533a
f4v10 <102195498+f4v10@users.noreply.github.com>
2024-06-08 23:26:47 -0500

    feat(command-and-query-services):  added command a query services referent to notifications.

---
+++ backend/Notifications/Application/Internal/CommandServices/NotificationCommandService.cs
@@ -0,0 +1,23 @@
+using backend.Notifications.Domain.Model.Aggregates;
+using backend.Notifications.Domain.Model.Commands;
+using backend.Notifications.Domain.Repositories;
+using backend.Notifications.Domain.Services;
+using backend.Shared.Domain.Repositories;
+
+namespace backend.Notifications.Application.Internal.CommandServices;
+
+public class NotificationCommandService(INotificationRepository notificationRepository, IUnitOfWork unitOfWork) : INotificationCommandService
+{
+    public async Task<Notification?> Handle(CreateNotificationCommand command)
+    {
+        var notification = new Notification(command.Id, command.Title, command.Description);
+        await notificationRepository.AddSync(notification);
+        await unitOfWork.CompleteAsync();
+        return notification;
+    }
+
+    public Task Handle(DeleteNotificationCommand command)
+    {
+        return notificationRepository.DeleteByIdAsync(command.Id);
+    }
+}
